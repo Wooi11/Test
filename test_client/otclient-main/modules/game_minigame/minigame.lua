694d86bfbf06b12ebac509a3bb2c535338c1581d
wooi <cnjfhr@gmail.com>
2024-04-23 02:58:02 +1200

    add module_minigame

--- test_client/otclient-main/modules/game_minigame/minigame.lua
+++ test_client/otclient-main/modules/game_minigame/minigame.lua
@@ -1,7 +1,9 @@
 miniGameButton = nil
 miniGameStartButton = nil
 miniGameWindow = nil
-miniGameOn = false
+startButton = nil
+jumpButton = nil
+oldPos = nil
 
 function init()
     g_ui.importStyle('minigamewindow')
@@ -10,13 +12,20 @@ function init()
     miniGameWindow = g_ui.createWidget('MiniGameWindow', rootWidget)
     miniGameWindow:hide()
 
+    startButton = miniGameWindow:getChildById("startButton")
+
+    jumpButton = miniGameWindow:getChildById("jumpButton")
+    jumpButton:hide()
+
 end
 
 function terminate()
 
     miniGameButton:destroy()
     miniGameWindow:destroy()
-
+    startButton:destroy()
+    jumpButton:destroy()
+    startButton = nil
     miniGameButton = nil
     miniGameWindow = nil
 
@@ -24,12 +33,14 @@ end
 
 function show()
     miniGameWindow:show()
-    miniGameOn = false; 
+    jumpButton:hide()
+    startButton:show()
 end
 
 function hide()
     miniGameWindow:hide()
-    miniGameOn = false;
+    jumpButton:hide();
+    startButton:hide();
 end
 
 function toggle()
@@ -41,11 +52,43 @@ function toggle()
 end
 
 function start()
-
+    addEvent(buttonMove)
+    jumpButton:show()
+    startButton:hide()
 end
 
 function cancel()
     miniGameWindow:hide()
-    miniGameOn = false;
+    jumpButton:hide()
+    startButton:show()
+end
+
+
+function buttonMove()
+    if jumpButton:isVisible() then
+        jumpButton:show()
+        addEvent(buttonMove, 1000)
+        windowPos = miniGameWindow:getPosition()
+        oldPos = jumpButton:getPosition()
+
+        local pos = oldPos
+        local rand = math.random(50, 400)
+        
+        pos.x = oldPos.x -1
+        if pos.x <= windowPos.x then
+            pos.x = windowPos.x + 400
+            pos.y = windowPos.y + rand
+        end
+        jumpButton:setPosition(pos)
+    end
+end
+
+function jump()
+    windowPos = miniGameWindow:getPosition()
+    local pos = windowPos
+    local rand = math.random(50, 400)
+    pos.x = windowPos.x + 400
+    pos.y = windowPos.y + rand
+    jumpButton:setPosition(pos)
 end
 
